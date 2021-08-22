c3d6c9eeaad30bfa41129fe837338b9f1d66f6bf
Veeresh <imveeresh6454@gmail.com>
2021-08-22 08:38:29 +0530

    Committed

---
+++ src/test/java/DemoTestCase.java
@@ -0,0 +1,18 @@
+import org.testng.annotations.Test;
+
+public class DemoTestCase 
+{
+	@Test
+	public void print1()
+	{
+		System.out.println("*******");
+	}
+	
+	
+	@Test
+	public void print2()
+	{
+		System.out.println("########");
+	}
+
+}
